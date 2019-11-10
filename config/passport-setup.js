58920da666917c725118779570d003907382e4fe
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2019-11-10 13:56:28 +0300

    add support to allow for cookie sending to browser

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,16 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const User = require('../models/user-model');
+
+passport.serializeUser((user, done)=> {
+  done(null, user.id); //stuff it to the cookie
+})
+
+passport.deserializeUser((id, done)=>{
+  User.findById(id).then((user)=> {
+    done(null, user);
+  });
+});
 passport.use(
   new GoogleStrategy({
     clientID: keys.google.clientID,
@@ -10,13 +20,23 @@ passport.use(
   },
   (accessToken, refreshToken, profile, done) => {
       //passport call back funtion is what really recieves profile info after the code-profile exchange
-      console.log('passport callback function fired');
-      console.log(profile);
-      new User({
-        username: profile.displayName,
-        googleId: profile.id
-      }).save().then((newUser)=> {
-        console.log('new user created'+ newUser);
-      })
+        // checkout if user already exists in our db
+        User.findOne({googleId: profile.id}).then((currentUser)=>{
+          if(currentUser) {
+            // already have the user
+             console.log("User is:"+ currentUser); 
+             done(null, currentUser);
+          } else {
+            //if not create user in our db
+            new User({
+              username: profile.displayName,
+              googleId: profile.id
+            }).save()
+              .then(newUser => {
+                console.log("new user created" + newUser);
+                done(null, newUser);
+              });
+          }
+        })    
   })
 );
